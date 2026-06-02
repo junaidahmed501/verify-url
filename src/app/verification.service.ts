50d243b505c5aae73d5d7933111e8d7e74804821
Junaid Ahmed <junaidahmed501@gmail.com>
2026-06-02 14:51:21 +0200

    feat: updated design as the previous one has gotten boring

--- src/app/verification.service.ts
+++ src/app/verification.service.ts
@@ -1,6 +1,6 @@
 import {inject, Injectable} from '@angular/core';
 import {UrlCheckResult} from './models';
-import {HttpClient, HttpErrorResponse} from '@angular/common/http';
+import {HttpClient} from '@angular/common/http';
 import {catchError, map, throwError} from 'rxjs';
 
 @Injectable({
